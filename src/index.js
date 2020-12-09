c582f3262a9cc92591f1e15826b80ff8dc4b4fd5
komalswami <komalsswami@gmail.com>
2020-12-09 20:03:05 +0530

    deploy my react app to github

--- src/index.js
+++ src/index.js
@@ -1,13 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import img1 from './imge1.jpeg'
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import BarChart from './components/BarChart';
+import LineChart from './components/LineChart';
+
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div>
+  <div>
+  <b><a href="#">Cockpit</a>  &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</b>
+    <b>My TasK &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</b> 
+     <b>My Profile &nbsp; &nbsp;</b>
+     <button class="button button1"> logout</button>
+     <hr></hr></div><br/>
+      <br/>
+      
+<div>
+&nbsp;&nbsp; &nbsp;&nbsp;<img src={img1} height="300" width="830" alt="logo" />
+   
+   <BarChart/><br/>
+  
+   </div>
+      </div>
+  ,
   document.getElementById('root')
 );
 
